57f755d36ffbffc951b4ec97d27ef38675834317
Andrei Doroganici <andrei.doroganici@iongroup.com>
2025-02-11 15:15:22 +0200

    added the functionality to show the selected page button from MainLayout, refactored HomeView, added logo

--- src/main/java/org/cedacri/pingpong/views/MainLayout.java
+++ src/main/java/org/cedacri/pingpong/views/MainLayout.java
@@ -1,98 +1,78 @@
 package org.cedacri.pingpong.views;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.html.H4;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.AfterNavigationObserver;
+import org.cedacri.pingpong.utils.ViewUtils;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @CssImport("./themes/ping-pong-tournament/main-layout.css")
-@Route("")
-public class MainLayout extends AppLayout {
+public class MainLayout extends AppLayout implements AfterNavigationObserver {
 
-    private final Button homeButton;
-    private final Button turneuButton;
-    private final Button jucatoriButton;
+    private List<Button> menuButtons;
+    private final List<String> routes = Arrays.asList("home", "tournaments", "players");
 
     public MainLayout() {
-        // Top bar
-//        logoutButton = new Button("Log out", VaadinIcon.SIGN_OUT.create());
-//        logoutButton.addClassName("logout-button");
-//        addToNavbar(logoutButton);
+        configureView();
+    }
 
-        // Drawer content (side-menu)
+    private void configureView() {
         setPrimarySection(Section.DRAWER);
 
-        // Logo & Admin Name section
-        H3 appName = new H3("TOURNAMENT");
-        Div logoSection = new Div();
-        logoSection.setWidthFull();
-        logoSection.addClassName("logo-section");
-        logoSection.getStyle().set("display", "flex");
-        logoSection.getStyle().set("align-items", "center");
-        logoSection.getStyle().set("justify-content", "center");
-        logoSection.add(appName);
+        H4 appName = new H4("TOURNAMENT");
+        Image logo = new Image("images/logo.png", "Tournament Logo");
+        logo.setWidth("160px");
+        logo.setHeight("auto");
 
-        homeButton = new Button("Home");
-        homeButton.addClassName("transparent-button");
-        homeButton.addClickListener(e -> getUI().ifPresent(ui -> ui.navigate("home")));
+        VerticalLayout logoLayout = ViewUtils.createVerticalLayout(FlexComponent.JustifyContentMode.START, logo, appName);
+        logoLayout.addClassName("logo-section");
 
-        turneuButton = new Button("Tournament");
-        turneuButton.addClassName("transparent-button");
-        turneuButton.addClickListener(e -> getUI().ifPresent(ui -> ui.navigate("tournaments")));
+        menuButtons = Arrays.asList(
+                ViewUtils.createButton("Home", "transparent-button", () -> navigateTo("home")),
+                ViewUtils.createButton("Tournament", "transparent-button", () -> navigateTo("tournaments")),
+                ViewUtils.createButton("Players", "transparent-button", () -> navigateTo("players"))
+        );
 
-        jucatoriButton = new Button("Players");
-        jucatoriButton.addClassName("transparent-button");
-        jucatoriButton.addClickListener(e -> getUI().ifPresent(ui -> ui.navigate("players")));
-
-        VerticalLayout menuItemsLayout = new VerticalLayout(homeButton, turneuButton, jucatoriButton);
+        VerticalLayout menuItemsLayout = ViewUtils.createVerticalLayout(FlexComponent.JustifyContentMode.START, menuButtons.toArray(new Button[0]));
         menuItemsLayout.addClassName("menu-items");
-        menuItemsLayout.setPadding(false);
-        menuItemsLayout.setSpacing(false);
 
-        VerticalLayout drawerLayout = new VerticalLayout(logoSection, menuItemsLayout);
+        VerticalLayout drawerLayout = ViewUtils.createVerticalLayout(FlexComponent.JustifyContentMode.START, logoLayout, menuItemsLayout);
         drawerLayout.addClassName("side-menu");
-        drawerLayout.setPadding(false);
-        drawerLayout.setSpacing(true);
+
         addToDrawer(drawerLayout);
     }
 
-    private void highlightActiveMenuItem(String route) {
-        homeButton.removeClassName("active");
-        turneuButton.removeClassName("active");
-        jucatoriButton.removeClassName("active");
+    private void navigateTo(String route) {
+        UI.getCurrent().navigate(route);
+    }
 
-        switch (route) {
-            case "home":
-                homeButton.addClassName("active");
-                break;
-            case "tournament":
-                turneuButton.addClassName("active");
-                break;
-            case "players":
-                jucatoriButton.addClassName("active");
-                break;
-            default:
-                break;
-        }
+    private void highlightActiveMenuItem(String route) {
+        int selectedIndex = routes.indexOf(route);
+        ViewUtils.highlightSelectedComponentFromComponentsList(menuButtons, selectedIndex, "selected");
     }
 
     @Override
-    protected void afterNavigation() {
-        super.afterNavigation();
-        String currentRoute = getCurrentRoute();
-        highlightActiveMenuItem(currentRoute);
+    public void afterNavigation(AfterNavigationEvent event) {
+        highlightActiveMenuItem(getCurrentRoute());
     }
 
     private String getCurrentRoute() {
-        return getUI()
-                .flatMap(ui -> ui.getInternals().getActiveViewLocation() != null
-                        ? Optional.of(ui.getInternals().getActiveViewLocation().getPath())
-                        : Optional.empty())
-                .orElse("");
+        return Optional.ofNullable(
+                    UI.getCurrent()
+                            .getInternals()
+                            .getActiveViewLocation()
+                            .getPath())
+                .orElse("home");
     }
 }
--- src/main/java/org/cedacri/pingpong/views/home/HomeView.java
+++ src/main/java/org/cedacri/pingpong/views/home/HomeView.java
@@ -1,91 +1,69 @@
 package org.cedacri.pingpong.views.home;
 
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.dependency.Uses;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
 import lombok.extern.slf4j.Slf4j;
 import org.cedacri.pingpong.entity.Tournament;
 import org.cedacri.pingpong.service.TournamentService;
 import org.cedacri.pingpong.utils.ViewUtils;
 import org.cedacri.pingpong.views.MainLayout;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Comparator;
 import java.util.List;
 
 @Slf4j
 @PageTitle("Home")
-@Route(value = "home", layout = MainLayout.class)
+@Route(value = "", layout = MainLayout.class)
+@RouteAlias(value = "home", layout = MainLayout.class)
 @CssImport("./themes/ping-pong-tournament/main-layout.css")
-@Uses(Icon.class)
 public class HomeView extends VerticalLayout {
 
     private final TournamentService tournamentService;
+    private final VerticalLayout tournamentContainer = new VerticalLayout();
 
-    private VerticalLayout tournamentContainer;
-
+    @Autowired
     public HomeView(TournamentService tournamentService) {
         this.tournamentService = tournamentService;
         log.info("HomeView initialized.");
 
-        initView();
-    }
-
-    private void initView() {
         setSizeFull();
         setPadding(true);
         setSpacing(true);
 
-        add(
-                ViewUtils.createHorizontalLayout(
-                        JustifyContentMode.BETWEEN,
-                        new H1("Home")
-                )
-        );
-
-        tournamentContainer = new VerticalLayout();
+        add(ViewUtils.createHorizontalLayout(FlexComponent.JustifyContentMode.BETWEEN, new H1("Home")));
         tournamentContainer.setSpacing(true);
         tournamentContainer.setHorizontalComponentAlignment(Alignment.STRETCH);
         add(tournamentContainer);
 
-        displayTournaments(tournamentService.findAll().toList());
+        displayTournaments();
     }
 
-    private void displayTournaments(List<Tournament> tournaments) {
+    private void displayTournaments() {
+        List<Tournament> tournaments = tournamentService.findAll().toList();
         log.info("Displaying {} tournaments", tournaments.size());
+
+        tournamentContainer.removeAll();
 
         if (tournaments.isEmpty()) {
             showNoTournamentFound();
             return;
         }
 
-        for (Tournament tournament : tournaments) {
+        tournaments.forEach(tournament -> {
             log.debug("Processed tournament {}", tournament);
-
-            TournamentSummaryComponent tournamentLayout = new TournamentSummaryComponent(tournamentService, tournament);
-            tournamentContainer.add(tournamentLayout);
-        }
-
+            tournamentContainer.add(new TournamentSummaryComponent(tournamentService, tournament));
+        });
     }
 
     private void showNoTournamentFound() {
-        tournamentContainer.removeAll(); // Clear any existing content
-
         H1 noTournamentsMessage = new H1("There are currently no tournaments");
-        noTournamentsMessage.getStyle()
-                .set("text-align", "center")
-                .set("width", "100%")
-                .set("color", "gray");
-
+        noTournamentsMessage.getStyle().set("text-align", "center").set("width", "100%").set("color", "gray");
         tournamentContainer.setAlignItems(Alignment.CENTER);
         tournamentContainer.add(noTournamentsMessage);
     }
-
-    private List<Tournament> getTournamentsList() {
-        return tournamentService.findAll().sorted(Comparator.comparing(Tournament::getCreatedAt).reversed()).toList();
-    }
-
 }
